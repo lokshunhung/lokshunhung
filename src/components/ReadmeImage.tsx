1b5e5e05f7d4b75f9af01c3bfddf63fc6fbeac28
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-11 22:19:10 +0800

    animate cat hands

--- src/components/ReadmeImage.tsx
+++ src/components/ReadmeImage.tsx
@@ -1,4 +1,4 @@
-import { CatHead, LHandDown, RHandDown } from '~/src/assets/cat';
+import { CatHead, LHandDown, LHandUp, RHandDown, RHandUp } from '~/src/assets/cat';
 import { dimensions } from '~/src/defs/values';
 import { Svg } from './Svg';
 
@@ -8,12 +8,12 @@ export const ReadmeImage = ({ style }: ReadmeImageProps) => {
     return (
         <Svg viewBoxWidth={dimensions.width} viewBoxHeight={dimensions.height} style={style}>
             <style>{require('bundle-text:./ReadmeImage.scss')}</style>
-
             <rect id="bg" />
-
-            <CatHead />
-            <LHandDown />
-            <RHandDown />
+            <CatHead id="head" />
+            <LHandUp id="l-hand-up" />
+            <LHandDown id="l-hand-down" />
+            <RHandUp id="r-hand-up" />
+            <RHandDown id="r-hand-down" />
         </Svg>
     );
 };
--- src/defs/values.ts
+++ src/defs/values.ts
@@ -2,3 +2,7 @@ export const dimensions = {
     width: 1000,
     height: 1000,
 };
+
+export const durations = {
+    animTick: 3,
+};
