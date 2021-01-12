5305969d8c447ed946d79fbe70d8e858d1629893
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-12 13:53:00 +0800

    add bongo

---
+++ src/assets/accessories/Bongo.tsx
@@ -0,0 +1,7 @@
+export const Bongo = () => {
+    return (
+        <g className="acc-bongo">
+            <image href={require('data-url:./Bongo.png')} />
+        </g>
+    );
+};
--- src/components/ReadmeImage.tsx
+++ src/components/ReadmeImage.tsx
@@ -1,9 +1,11 @@
 import { CatHead, LPawDown, LPawUp, RPawDown, RPawUp } from '~/src/assets/cat';
 import { dimensions } from '~/src/defs/values';
+import { Bongo } from '../assets/accessories/Bongo';
 import { Svg } from './Svg';
 
 const inlineStyleSheet = [
     require('bundle-text:~/src/assets/cat/index.scss'),
+    require('bundle-text:~/src/assets/accessories/Bongo.scss'),
     require('bundle-text:./ReadmeImage.scss'),
     require('bundle-text:./ReadmeImage.paws.scss'),
 ].join('\n\n');
@@ -16,6 +18,7 @@ export const ReadmeImage = ({ style }: ReadmeImageProps) => {
             <style>{inlineStyleSheet}</style>
             <rect id="bg" />
             <line x1={0} y1={475} x2={1000} y2={710} strokeWidth={10} stroke="#000" />
+            <Bongo />
             <CatHead id="head" />
             <LPawUp id="l-paw-up" />
             <LPawDown id="l-paw-down" />
