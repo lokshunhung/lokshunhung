4f3111ec320dddea614cc7e5eca57e85ab9e6fc9
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-01-17 13:46:47 +0800

    prepare for adding more character

---
+++ src/assets/cat/characters/index.ts
@@ -0,0 +1,20 @@
+export type Character = 'BongoCat';
+
+export type CharacterSet = {
+    inlineStyles: string;
+    CharacterComponent: React.ComponentType;
+};
+
+const createStyles = (styles: string) => [require('bundle-text:./common.scss'), styles].join('\n');
+
+const NullComponent = () => null;
+
+export const getCharacter = (character: Character): CharacterSet => {
+    switch (character) {
+        case 'BongoCat':
+            return {
+                inlineStyles: createStyles(require('bundle-text:./BongoCat.scss')),
+                CharacterComponent: NullComponent,
+            };
+    }
+};
--- src/components/ReadmeImage.tsx
+++ src/components/ReadmeImage.tsx
@@ -1,5 +1,5 @@
 import { AccessoryType, getAccessory } from '~/src/assets/accessories';
-import { CatFace, CatHead, LPawDown, LPawUp, RPawDown, RPawUp } from '~/src/assets/cat';
+import { CatFace, CatHead, getCharacter, LPawDown, LPawUp, RPawDown, RPawUp } from '~/src/assets/cat';
 import { dimensions } from '~/src/defs/values';
 import { Svg } from './Svg';
 
@@ -7,10 +7,11 @@ type ReadmeImageProps = { style?: React.CSSProperties; accessoryType: AccessoryT
 
 export const ReadmeImage = ({ style, accessoryType }: ReadmeImageProps) => {
     const { inlineStyles, description, BeforeComponent, AfterComponent } = getAccessory(accessoryType);
+    const { inlineStyles: inlineStyles2, CharacterComponent } = getCharacter('BongoCat');
 
     const inlineStyleSheet = [
         inlineStyles,
-        require('bundle-text:~/src/assets/cat/index.scss'),
+        inlineStyles2,
         require('bundle-text:./ReadmeImage.scss'),
         require('bundle-text:./ReadmeImage.paws.scss'),
     ].join('\n\n');
@@ -20,6 +21,9 @@ export const ReadmeImage = ({ style, accessoryType }: ReadmeImageProps) => {
             <style>{inlineStyleSheet}</style>
 
             <rect id="background" />
+
+            <CharacterComponent />
+
             <line id="table" x1={0} y1={475} x2={1000} y2={710} />
 
             <BeforeComponent />
